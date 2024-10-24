b4cddf45d6dcb641b11684eb840216a3152fdfd9
baig78 <mirzanaimathbaig@gmail.com>
2024-10-24 13:14:13 +0530

    first commit

---
+++ src/shared/Header/Header.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './Header.css'; // Import the CSS file
+
+const Header = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <a href="/">
+          <img src="../../../images/logo_1.PNG" alt="Logo" />
+        </a>
+      </div>
+
+      <nav className={isMobile ? "nav-links-mobile" : "nav-links"}>
+        <a href="#home">Features</a>
+        <a href="#services">Demo</a>
+        <a href="#about">Use Cases</a>
+        <a href="#contact">Solutions</a>
+        <a href="#contact">Case Studies</a>
+        <a href="#contact">Portfolio</a>
+        <a href="#contact">Resources</a>
+        <button className="contact-btn">Contact Now</button>
+      </nav>
+
+      <div className="mobile-menu-icon" onClick={() => setIsMobile(!isMobile)}>
+        {isMobile ? <FaTimes /> : <FaBars />}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
